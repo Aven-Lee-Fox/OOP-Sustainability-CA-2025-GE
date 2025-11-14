4d929986c7d0f45f2ce4bed9f3202f3355bf9baf
Joseph Moiselle <moisellejoseph05@gmail.com>
2025-11-14 12:41:33 +0000

    Joseph's UI plus Main menu UI created

---
+++ OOP-CA-App/src/oop/ca/app/OOPCAApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop.ca.app;
+
+/**
+ *
+ * @author Joseph Moiselle 24308453
+ */
+public class OOPCAApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainGUI myGUI = new MainGUI();
+        myGUI.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
